c57906f65724e3f749e37ef94f44135f05cfd5af
Chris Burgess <chris@giantrobot.co.nz>
2017-02-25 11:07:00 +1300

    Document configuration for bolts.

---
+++ example-config.sh
@@ -0,0 +1,18 @@
+#
+# Copy this file to .config.sh and set the variables below.
+#
+
+export MATTERMOST_HOST=chat.civicrm.org
+export MATTERMOST_GROUP=civicrm
+export MATTERMOST_USER=yourbotname
+export MATTERMOST_PASSWORD=yourbotpass
+export MATTERMOST_LOG_LEVEL=debug
+# export PORT=9099
+
+export HUBOT_TWITTER_CONSUMER_KEY=
+export HUBOT_TWITTER_CONSUMER_SECRET=
+export HUBOT_TWITTER_ACCESS_TOKEN_KEY=
+export HUBOT_TWITTER_ACCESS_TOKEN_SECRET=
+export HUBOT_TWITTER_MENTION_QUERY="#civicrm OR from:civicrm"
+export HUBOT_TWITTER_MENTION_ROOM=x-bot-test
+export HUBOT_TWITTER_MAX_TWEETS=5
