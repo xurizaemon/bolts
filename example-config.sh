9ac86c6b30dae6c8fc382ba8990aa4130a5d0767
Chris Burgess <chris@giantrobot.co.nz>
2017-03-09 21:25:43 +1300

    Refs #1, add `#civicamp`

--- example-config.sh
+++ example-config.sh
@@ -4,15 +4,25 @@
 
 export MATTERMOST_HOST=chat.civicrm.org
 export MATTERMOST_GROUP=civicrm
-export MATTERMOST_USER=yourbotname
-export MATTERMOST_PASSWORD=yourbotpass
-export MATTERMOST_LOG_LEVEL=debug
+export MATTERMOST_USER=xxx
+export MATTERMOST_PASSWORD=xxx
+export MATTERMOST_LOG_LEVEL=info
+export MATTERMOST_IGNORE_USERS=
 # export PORT=9099
 
-export HUBOT_TWITTER_CONSUMER_KEY=
-export HUBOT_TWITTER_CONSUMER_SECRET=
-export HUBOT_TWITTER_ACCESS_TOKEN_KEY=
-export HUBOT_TWITTER_ACCESS_TOKEN_SECRET=
-export HUBOT_TWITTER_MENTION_QUERY="#civicrm OR from:civicrm"
-export HUBOT_TWITTER_MENTION_ROOM=x-bot-test
+export HUBOT_BASE_URL=http://localhost:9099
+export HUBOT_FACTOID_PREFIX='?!'
+export HUBOT_JIRA_URL=https://issues.civicrm.org/jira
+export HUBOT_JIRA_USER=xxx
+export HUBOT_JIRA_PASSWORD=xxx
+export HUBOT_MAIL_LISTENER_ROOMS=x-bot-test
+export HUBOT_MAIL_LISTENER_USERNAME=xxx
+export HUBOT_MAIL_LISTENER_PASSWORD=xxx
+export HUBOT_MAIL_LISTENER_HOST=imap.gmail.com
+export HUBOT_TWITTER_CONSUMER_KEY=xxx
+export HUBOT_TWITTER_CONSUMER_SECRET=xxx
+export HUBOT_TWITTER_ACCESS_TOKEN_KEY=xxx
+export HUBOT_TWITTER_ACCESS_TOKEN_SECRET=xxx
+export HUBOT_TWITTER_MENTION_QUERY="#civicrm OR #civicamp OR from:civicrm OR @civicrm"
+export HUBOT_TWITTER_MENTION_ROOM=socialmedia
 export HUBOT_TWITTER_MAX_TWEETS=5
